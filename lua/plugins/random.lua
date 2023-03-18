9fe9e5d4f05007728356a531bfacb8606df615ed
Jonas Dujava <jonas.dujava@gmail.com>
2023-03-18 19:20:29 +0100

    automatic resizing of windows

--- lua/plugins/random.lua
+++ lua/plugins/random.lua
@@ -78,5 +78,23 @@ return {
         'dstein64/vim-startuptime',
         cmd = 'StartupTime',
         keys = {{'<A-S>', '<CMD>StartupTime --tries 10<CR>'}},
-    }
+    },
+
+    -- auto-resize windows
+    {
+        "anuvyklack/windows.nvim",
+        event = "WinNew",
+        dependencies = {
+            { "anuvyklack/middleclass" },
+            { "anuvyklack/animation.nvim", enabled = false },
+        },
+        keys = { { "<leader>Z", "<cmd>WindowsMaximize<cr>", desc = "Zoom" } },
+        config = function()
+            vim.o.winwidth = 5
+            vim.o.equalalways = false
+            require("windows").setup({
+                animation = { enable = false, duration = 150 },
+            })
+        end,
+    },
 }
