789dace71f01e77196917c5b18bfb093b847ea76
Jonas Dujava <jonas.dujava@gmail.com>
2023-06-08 21:17:47 +0200

    inlay hints

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -7,6 +7,7 @@ return {
             { 'folke/neodev.nvim', opts = true },
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
+            'lvimuser/lsp-inlayhints.nvim',
         },
         opts = {
             -- options for vim.diagnostic.config()
@@ -203,5 +204,26 @@ return {
             init_check = false,
             path = ".ltex",
         },
+    },
+
+    -- inlay hints
+    {
+        "lvimuser/lsp-inlayhints.nvim",
+        branch = "anticonceal",
+        event = "LspAttach",
+        opts = {},
+        config = function(_, opts)
+            require("lsp-inlayhints").setup(opts)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {}),
+                callback = function(args)
+                    if not (args.data and args.data.client_id) then
+                        return
+                    end
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    require("lsp-inlayhints").on_attach(client, args.buf)
+                end,
+            })
+        end,
     },
 }
