25889872394863b30b69d43ec1868b9fa509e99a
Jonas Dujava <jonas.dujava@gmail.com>
2023-06-22 14:51:27 +0200

    lazy loading of autocommands and keybinds

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,16 +1,20 @@
 -- load settings, helpers, autocommands, and keybindings
 require('jd.settings')
-require('jd.helpers')
-require('jd.aucmds')
-require('jd.bindings')
 
--- -- can also load binding later
--- vim.api.nvim_create_autocmd('User', {
---     pattern = 'VeryLazy',
---     callback = function()
---         require('jd.bindings')
---     end,
--- })
+if vim.fn.argc(-1) == 0 then
+    -- autocmds and keymaps can wait to load
+    vim.api.nvim_create_autocmd('User', {
+        group = vim.api.nvim_create_augroup('jd_core', { clear = true }),
+        callback = function()
+            require('jd.aucmds')
+            require('jd.bindings')
+        end,
+    })
+else
+    -- load them now so they affect the opened buffers
+    require('jd.aucmds')
+    require('jd.bindings')
+end
 
 return {
     { 'folke/lazy.nvim', version = '*' },
