fd6100b9951865aec4180281b94fbd26f56cdc6c
Jonas Dujava <jonas.dujava@gmail.com>
2023-06-22 14:41:00 +0200

    update autocommands

--- lua/jd/aucmds.lua
+++ lua/jd/aucmds.lua
@@ -1,57 +1,84 @@
+-- This file is automatically loaded by plugins.core
 local au = vim.api.nvim_create_autocmd
-local gr = vim.api.nvim_create_augroup
-
-local auto_updates = gr('auto_updates', {})
-local core = gr('core', {})
-local term = gr('term', {})
+local function gr(name)
+    return vim.api.nvim_create_augroup('jd_' .. name, { clear = true })
+end
 
 -- Check if we need to reload the file when it changed
 au({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+    group = gr('checktime'),
     command = 'checktime',
-    group = core,
 })
 
 -- Automatically deletes all trailing whitespace on save
 au('BufWritePre', {
+    group = gr('delete_trailing'),
     command = [[%s/\s\+$//e]],
-    group = gr('delete_trailing', {}),
 })
 
--- resize panes when host window is resized
+-- Resize panes when host window is resized
 au('VimResized', {
+    group = gr('resize_splits'),
     command = [[wincmd =]],
-    group = core,
 })
 
--- " Disables automatic commenting on newline - Commentary
+-- Disable automatic commenting on newline
 au({ 'BufEnter', 'BufRead' }, {
+    group = gr('formatoptions'),
     callback = function()
         vim.opt.formatoptions = vim.opt.formatoptions
             + 'c' -- In general, I like it when comments respect textwidth
             - 'o' -- O and o, don't continue comments
             + 'r' -- But do continue when pressing enter.
     end,
-    group = core,
 })
 
 au('TextYankPost', {
+    group = gr('highlight_yank'),
     callback = function()
         vim.highlight.on_yank({ higroup = 'Visual', on_visual = false })
     end,
-    group = core,
+})
+
+-- Go to last loc when opening a buffer
+au('BufReadPost', {
+    group = gr('last_loc'),
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
+au('TermOpen', {
+    group = gr('term_filetype'),
+    pattern = 'term://*',
+    callback = function()
+        vim.bo.filetype = 'term'
+    end,
+})
+au('TermOpen', {
+    group = gr('term_init'),
+    callback = function()
+        vim.cmd('startinsert')
+        vim.keymap.set('n', '<A-x>', '<cmd>bd!<cr>', { buffer = true })
+    end,
 })
 
 -- Auto updates
 local autoupdate = function(pattern, command)
     au('BufWritePost', {
+        group = gr('auto_updates'),
         pattern = pattern,
         callback = function()
             require('jd.cmds').term_execute(command)
         end,
-        group = auto_updates,
     })
 end
 
+--stylua: ignore start
 autoupdate({ '*/shell/folders', 'shortcuts' }, [[shortcuts]])
 autoupdate('*/st-jd/config.def.h',             [[make && sudo make install]])
 autoupdate('*/dwm-jd/dwm.c',                   [[make && sudo make install]])
@@ -61,13 +88,3 @@ autoupdate('sxhkdrc',                          [[pkill -USR1 sxhkd]])
 autoupdate('dunstrc',                          [[killall dunst; setsid dunst >/dev/null 2>&1 &]])
 autoupdate('*praktikum/*plot.gnu',             [[gnuplot plot.gnu > loggg.txt]])
 autoupdate('fonts.conf',                       [[fc-cache]])
-
-au('TermOpen', {
-    pattern = 'term://*',
-    command = [[setfiletype term]],
-    group = term,
-})
-au('TermOpen', {
-    command = [[startinsert | nnoremap <buffer> <A-x> <CMD>bd!<CR>]],
-    group = term,
-})
