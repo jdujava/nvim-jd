c71127df8c0d220a592d3958c3fb8b7382011cc8
Jonas Dujava <jonas.dujava@gmail.com>
2024-06-18 20:52:00 +0200

    properly setup lazydev
    thanks LazyVim
    
    Signed-off-by: Jonas Dujava <jonas.dujava@gmail.com>

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -274,14 +274,14 @@ return {
     {
         'folke/lazydev.nvim',
         ft = 'lua',
-        opts = function()
-            return {
-                library = {
-                    uv = 'luvit-meta',
-                    lazyvim = 'LazyVim',
-                },
-            }
-        end,
+        cmd = 'LazyDev',
+        opts = {
+            library = {
+                { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+                { path = 'LazyVim', words = { 'LazyVim' } },
+                { path = 'lazy.nvim', words = { 'LazyVim' } },
+            },
+        },
     },
     -- Manage libuv types with lazy. Plugin will never be loaded
     { 'Bilal2453/luvit-meta', lazy = true },
