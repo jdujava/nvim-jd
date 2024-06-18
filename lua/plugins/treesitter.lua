0215e94ccb5d0ebb0355764dde26201b657dfe96
Jonas Dujava <jonas.dujava@gmail.com>
2024-06-18 20:52:27 +0200

    add treesitter textobject moves
    thanks LazyVim
    
    Signed-off-by: Jonas Dujava <jonas.dujava@gmail.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -76,6 +76,14 @@ return {
                         ['ia'] = { query = '@parameter.inner', desc = 'Select inner parameter' },
                     },
                 },
+                -- stylua: ignore
+                move = {
+                    enable = true,
+                    goto_next_start     = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+                    goto_next_end       = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+                    goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+                    goto_previous_end   = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+                },
             },
         },
         ---@param opts TSConfig
