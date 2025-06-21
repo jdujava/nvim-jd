1af76183de36db84309d66c4fdefa489ce7c2bdb
Jonas Dujava <jonas.dujava@gmail.com>
2025-06-21 17:30:54 +0200

    chore(treesitter): pin master branch for now
    Signed-off-by: Jonas Dujava <jonas.dujava@gmail.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        branch = 'master', -- for now
         event = 'VeryLazy',
         lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
         build = ':TSUpdate',
