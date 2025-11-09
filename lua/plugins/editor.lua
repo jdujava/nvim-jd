135ef94be93862ecb2f027ee7b44d444f906b167
Jonas Dujava <jonas.dujava@gmail.com>
2025-11-09 16:14:48 +0100

    chore(matchup): update
    Signed-off-by: Jonas Dujava <jonas.dujava@gmail.com>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -104,8 +104,6 @@ return {
         event = 'BufReadPost',
         init = function()
             vim.o.matchpairs = '(:),{:},[:],<:>'
-        end,
-        config = function()
             vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
             vim.g.matchup_override_vimtex = 1
             vim.g.matchup_matchparen_deferred = 1
